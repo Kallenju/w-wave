5ba6de9fa36dd8b64c5542321ba4efec54610410
Konstantin Solovev <kostyasolovyov54@gmail.com>
2023-11-07 23:05:46 +0400

    update

--- webpack_configs/webpack.production.config.js
+++ webpack_configs/webpack.production.config.js
@@ -5,127 +5,121 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: path.resolve(__dirname, '..', 'src', 'index.ts'),
+  entry: path.resolve(__dirname, '..', 'src', 'index.ts'),
 
-	output: {
-		filename: '[name].[fullhash:20].js',
-		publicPath: `/${process.env.PUBLIC_PATH ?? ''}`,
+  output: {
+    filename: '[name].[fullhash:20].js',
+    publicPath: `/${process.env.PUBLIC_PATH ?? ''}`,
     clean: true,
-	},
+  },
 
-	plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(
-        __dirname,
-        '..',
-        'src',
-        'index.ejs',
-      ),
+      template: path.resolve(__dirname, '..', 'src', 'index.ejs'),
       inject: true,
       minify: true,
     }),
-		new MiniCssExtractPlugin({
-			filename: '[name].[contenthash].css',
-			chunkFilename: '[id].[contenthash].css',
-		}),
-	],
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
+  ],
 
-	module: {
-		rules: [
-			{
-				test: /\.ejs$/,
-				loader: 'ejs-loader',
-				options: {
-				  esModule: false,
-				},
-			},
-			{
-				test: /\.ts$/,
-				include: path.resolve(__dirname, '..', 'src'),
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							babelrc: false,
-							configFile: path.resolve(
-								__dirname,
-								'..',
-								'babel_configs',
-								'babel.production.config.js',
-							),
-						},
-					},
-				],
-			},
-			{
-				test: /\.css$/,
-				include: path.resolve(__dirname, '..', 'src'),
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							importLoaders: 1,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								config: path.resolve(
-									__dirname,
-									__dirname,
-									'..',
-									'postcss_configs',
-									'postcss.production.config.js',
-								),
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /\.(woff|woff2)$/,
-				include: path.resolve(__dirname, '..', 'src', 'assets', 'fonts'),
-				type: 'asset/resource',
-				generator: {
-					filename: 'fonts/[name][ext]',
-				},
-			},
-			{
-				test: /\.(png|svg|jpg|gif|webp)$/,
-				include: path.resolve(__dirname, '..', 'src'),
-				type: 'asset/resource',
-				generator: {
-					filename: 'img/[name][ext]',
-				},
-			},
-			{
-				test: /\.ico$/,
-				include: path.resolve(__dirname, '..', 'src', 'favicon.ico'),
-				type: 'asset/resource',
-				generator: {
-					filename: '[name][ext]',
-				},
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        loader: 'ejs-loader',
+        options: {
+          esModule: false,
+        },
+      },
+      {
+        test: /\.ts$/,
+        include: path.resolve(__dirname, '..', 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              configFile: path.resolve(
+                __dirname,
+                '..',
+                'babel_configs',
+                'babel.production.config.js',
+              ),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, '..', 'src'),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: path.resolve(
+                  __dirname,
+                  '..',
+                  'postcss_configs',
+                  'postcss.production.config.js',
+                ),
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        include: path.resolve(__dirname, '..', 'src', 'assets', 'fonts'),
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif|webp)$/,
+        include: path.resolve(__dirname, '..', 'src'),
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+      {
+        test: /\.ico$/,
+        include: path.resolve(__dirname, '..', 'src', 'favicon.ico'),
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]',
+        },
+      },
+    ],
+  },
 
-	devtool: 'source-map',
+  devtool: 'source-map',
 
-	target: ['browserslist'],
+  target: ['browserslist'],
 
-	mode: 'production',
+  mode: 'production',
 
-	optimization: {
-		minimize: true,
-		splitChunks: {
-			chunks: 'all',
-		},
-		runtimeChunk: {
-			name: (entrypoint) => `runtime~${entrypoint.name}`,
-		},
-	},
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: {
+      name: (entrypoint) => `runtime~${entrypoint.name}`,
+    },
+  },
 };
